acae0ec6d02f186b7fd2828f27853ddeca326393
helabenkhalfallah <hela@upperskills.com>
2019-05-12 20:30:19 +0200

    [HBElements] update button

--- src/components/button/HBButton.jsx
+++ src/components/button/HBButton.jsx
@@ -1,14 +1,53 @@
+import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 
-const HBButton = styled.button`
-  background: ${props => (props.primary ? 'palevioletred' : 'white')};
-  color: ${props => (props.primary ? 'white' : 'palevioletred')};
+const HBButtonStyle = styled.button`
+  background: ${props => (props.type === 'primary' ? 'palevioletred' : 'white')};
+  color: ${props => (props.type === 'primary' ? 'white' : 'palevioletred')};
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
   border: 2px solid palevioletred;
   border-radius: 3px;
 `;
+
+const HBButton = ({
+  title,
+  type,
+  disabled,
+  onClick,
+}) => {
+  return (
+    <HBButtonStyle
+      type={type}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {title}
+    </HBButtonStyle>
+  );
+};
+
+/**
+ * propType validation
+ */
+HBButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['primary', 'secondary']),
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+
+/**
+ * propType default values
+ */
+HBButton.defaultProps = {
+  type: 'primary',
+  disabled: false,
+  onClick: null,
+};
+
 
 export default HBButton;
